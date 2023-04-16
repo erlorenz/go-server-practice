d47bbf8265ad81fe976b6371ab41466a99f119a6
Erik Lorenz <erik@eriklorenz.dev>
2023-04-16 09:03:24 -0500

    Got base server running

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+)
 
-
+type config struct {
+	addr string
+}
 
 func main() {
-	fmt.Println("Hello world")
+	var cfg config
+
+	flag.StringVar(&cfg.addr, "addr", ":3000", "The port on which the server will listen")
+	flag.Parse()
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Health is good!")
+	})
+
+	log.Printf("Listening on port %s", cfg.addr)
+	err := http.ListenAndServe(cfg.addr, mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
